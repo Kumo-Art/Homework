9b46a82e1a26636c42e333699c8e2e449b254c4f
Kumo-Art <brandonlangehennig67@gmail.com>
2026-09-09 23:37:23 -0800

    Mini Challenge  2

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿//Brandon Langehennig
+//Mini Challenge 2
+// I entered two numbers and found the sum of them
+//Peer Name:
+//Peer Review:
+
+
+Console.WriteLine("Enter the first number: ");
+
+string num1 = Console.ReadLine();
+
+
+Console.WriteLine("Enter the first number: ");
+
+
+
+Console.WriteLine("Enter the second number: ");
+
+string num2 = Console.ReadLine();
+
+
+Console.WriteLine("Enter the second number: ");
+
+
+
+Console.WriteLine("The sum of the two numbers is: " + (Convert.ToInt32(num1) + Convert.ToInt32(num2)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
