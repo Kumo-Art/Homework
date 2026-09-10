f557ac33b9c3be31acd5f4e59234f14b3cdfc45d
Kumo-Art <brandonlangehennig67@gmail.com>
2026-09-09 23:57:38 -0800

    Mini Challenge 3

---
+++ Mini Challenge 3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Brandon Langehennig
+//Mini Challenge 3
+//Ask and answer two questions and make output a sentence
+//Peer Name:
+//Peer Review:
+
+
+Console.WriteLine("What is your Name?");
+
+string name = Console.ReadLine();
+
+
+Console.WriteLine("What time did you wake up?");
+
+string time = Console.ReadLine();
+
+
+Console.WriteLine($"Hello {name}, you woke up at {time}.");
