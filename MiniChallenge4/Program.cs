36f0895a5be6e90fdb12cd0ed87661963c3aa52f
Kumo-Art <brandonlangehennig67@gmail.com>
2026-09-10 00:29:44 -0800

    MiniChallenge4

---
+++ MiniChallenge4/Program.cs
@@ -0,0 +1,28 @@
+﻿//Brandon Langehennig
+//Mini Challenge 4
+// determine which value of 2 entries is greater
+//Peer name:
+//Peer review:
+
+
+
+
+
+
+Console.WriteLine("Enter a number: ");
+
+string input1 = Console.ReadLine();
+
+
+if (input1 >= input2) {
+    Console.WriteLine("input1 is greater");
+}
+ else if (input1 <= input2) {
+    
+    Console.WriteLine("input2 is greater");
+}  
+
+
+Console.WriteLine("Enter second number: ");
+
+string input2 = Console.ReadLine();
